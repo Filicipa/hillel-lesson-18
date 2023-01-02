1ef8205aff76ac38dc1c517cc5b21969d2889465
Alex Filatov <filicipa@gmail.com>
2023-01-03 01:06:14 +0200

    Fixed

--- lesson-18.py
+++ lesson-18.py
@@ -1,36 +1,40 @@
 import requests
-import datetime
-url = "https://baconipsum.com/api?type=meat-and-filler"
+from datetime import datetime
+import sys
 
-paragraphs = list()
+def get_paragraphs_amount():
+    p = 5
+    try:
+        p_input = input("Enter amount of paragraphs: ")
+        if int(p_input) > 0:
+            p = int(p_input)
+    except:
+        print("Something went wrong with paragraphs or it was 0")
+        sys.exit()
+    return p
 
-for x in range(5):
-    response = requests.get(url)
-    paragraphs.append(response.text)
 
-for elements in paragraphs:
-    print(elements)
+paragraphs_count = get_paragraphs_amount()
+
+url = f"https://baconipsum.com/api/?type=meat-and-filler&paras={paragraphs_count}&format=text"
+
+response = requests.get(url)
+text = response.text
+paragraphs = text.split("\n\n")
+print(paragraphs)
 
 print("-----NOW REVERSE-----")
-
 paragraphs.reverse()
 for elements in paragraphs:
     print(elements)
 
-word = "pancetta"
-amount_pancetta = 0
-for elements in paragraphs:
-    if word in elements.lower():
-        amount_pancetta = amount_pancetta + 1
+amount_pancetta = sum(1 if "pancetta" in e.lower() else 0 for e in paragraphs)
 
 print(f"Number of paragraphs with pancetta: {amount_pancetta}")
 
-date_now = datetime.datetime.now()
+date_now = datetime.now()
 with open("lesson-18.txt", 'w', encoding='utf8') as f:
-    f.write("Alexander " f'{date_now}\n' 
+    f.write("Alexander " f'{date_now}\n'
             f'{amount_pancetta}\n')
-f.close()
-
-with open("lesson-18.txt", 'a', encoding='utf8') as f:
     f.writelines(f'{elements}\n' for elements in paragraphs)
 f.close()
